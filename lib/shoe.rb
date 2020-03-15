d2889ac11b2e94e7f1c9647b6cd7e2ae6049369a
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 04:04:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -25,22 +25,10 @@ def condition=(condition)
  end
 
  def condition
-  #    if cobble == "new"
-  # end
+
    @condition
  end
 
-# def condition(condition)
-#   @condition
-#   # puts "new"
-#   # it 'makes the shoe\'s condition new' do
-#   #   shoe.condition = "old"
-#   #   shoe.cobble
-#   #   expect(shoe.condition).to eq("new")
-#   #
-#   end
 
 end
-#
-# Shoe.condition
->>>>>>> 5c4003b47b532fcaff9269706915775fc252ed9a
+
