82ec1b9864544f3544693c24d0427e2a9ae9790e
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 04:03:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,7 +12,35 @@ class Shoe
 
 def cobble
   puts "Your shoe is as good as new!"
+<<<<<<< HEAD
   @condition = "new"
 end
 
 end
+=======
+end
+
+def condition=(condition)
+   @condition = condition
+ end
+
+ def condition
+  #    if cobble == "new"
+  # end
+   @condition
+ end
+
+# def condition(condition)
+#   @condition
+#   # puts "new"
+#   # it 'makes the shoe\'s condition new' do
+#   #   shoe.condition = "old"
+#   #   shoe.cobble
+#   #   expect(shoe.condition).to eq("new")
+#   #
+#   end
+
+end
+#
+# Shoe.condition
+>>>>>>> 5c4003b47b532fcaff9269706915775fc252ed9a
